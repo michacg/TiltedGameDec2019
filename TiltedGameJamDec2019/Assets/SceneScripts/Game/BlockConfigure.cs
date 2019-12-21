933bcf4ad328caa59e423487b56836a19975d4f4
Richard Hsu <shengwh1@uci.edu>
2019-12-21 15:00:39 -0800

    procedure

---
+++ TiltedGameJamDec2019/Assets/SceneScripts/Game/BlockConfigure.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockConfigure : MonoBehaviour
+{
+    private float weight;
+    
+    public void ChangeWeight(float givenWeight)
+    {
+        weight = givenWeight;
+    }
+
+}
---
+++ TiltedGameJamDec2019/Assets/SceneScripts/Game/PlatformGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+    [Header("Inspector Header")]
+        [SerializeField]
+        GameObject platform;
+
+        [SerializeField]
+        List<GameObject> blocks = new List<GameObject>();
+
+        [SerializeField]
+        int platformLen = 10;
+
+    private float xSpawnValue;
+
+    private void Awake()
+    {
+        xSpawnValue = Camera.main.transform.position.x - (Camera.main.pixelWidth / 2);
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void SpawnPlatform()
+    {
+        float ySpawnValue = Camera.main.transform.position.y + Random.Range( Camera.main.pixelHeight, 2 * Camera.main.pixelHeight);
+        Instantiate(platform, new Vector2(xSpawnValue, ySpawnValue), Quaternion.identity);
+
+        for (int num = 0; num < platformLen; num++)
+        {
+            int blockIndex = Random.Range(0, blocks.Count);
+            float xBlock = xSpawnValue + (blocks[0].GetComponent<Sprite>().rect.width) * (num);
+
+            Instantiate(blocks[blockIndex], new Vector2(xBlock, ySpawnValue), Quaternion.identity);
+            
+        }
+
+    }
+
+    private void Destroy()
+    {
+        
+    }
+}
